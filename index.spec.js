80537406dabf574d89ed47649136c43ed9573a6c
r0neel <ronbhagwakar@gmail.com>
2022-01-21 15:46:20 +0000

    add more tests

--- index.spec.js
+++ index.spec.js
@@ -7,5 +7,33 @@ describe('GET /', function(){
             .get('/')
             .expect(200,done);
     })
+    test('responds with 404 when file not found',function(done){
+        request(app)
+            .get('/random')
+            .except(404,done); 
+    })
+    test('error 418 im a teapot', function(done){
+        request(app)
+        .get('/hack')
+        .expect(418,done);
+    })
+    test('responds with json object',function(done){
+        request(app)
+            .get('/city')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(done);
+    })
+    test('retrieves a time by city name', function(done){
+        request(app)
+            .get('city/london')
+            .expect(200)
+            .expect({id: , name: London, timeZone: ,time: },done)
+    })
+    test('responds to delete / with status 204', (done) => {
+        request(app)
+            .delete('/city/')
+            .expect(204, done);
+    });
 })
 
